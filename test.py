25c096544097443f1024715b73b5763df82cde1c
tudou <tudouzhuang@qq.com>
2024-08-02 13:10:19 +0800

    the first test

---
+++ test.py
@@ -0,0 +1,12 @@
+def insert_into_all(item, nested_list):
+    """Return a new list consisting of all the lists in nested_list,
+    but with item added to the front of each. You can assume that
+     nested_list is a list of lists.
+
+    >>> nl = [[], [1, 2], [3]]
+    >>> insert_into_all(0, nl)
+    [[0], [0, 1, 2], [0, 3]]
+    """
+    "*** YOUR CODE HERE ***"
+    for element in nested_list:
+        element.insert(0,item)
